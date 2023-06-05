df65cd12d172ac998b12b78827c6a3ff877eb28b
mariyadavydova <mrs.mariya.davydova@gmail.com>
2023-06-05 11:15:02 +0000

    pyairtable connected

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import os
+
+AIRTABLE_API_KEY = os.environ['AIRTABLE_API_KEY']
+BASE_ID = 'appvOnl6DnnKj8fVM'
+TABLE_NAME = 'Automagic'
+
+from pyairtable import Table
+
+table = Table(AIRTABLE_API_KEY, BASE_ID, TABLE_NAME)
+rows = table.all()
+for row in rows:
+  print(row)
+  id = row['id']
+  status = row['fields']['Status']
+  table.update(id, {'Status': f'{status}'})
+
